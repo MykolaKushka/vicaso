f7bc225171e4a38e5f84362b405d4a32ae2b7084
Mykola Kushka <Mykola.Kushka@gmail.com>
2023-08-03 00:42:52 +0300

    Const for map

--- src/js/nextBtn.js
+++ src/js/nextBtn.js
@@ -9,11 +9,13 @@ const nextBtn = () => {
 
     let formItems = document.querySelectorAll('.form-item-in.d-none');
 
+    const map = document.getElementById('map');
+
     nextBtn.addEventListener('click', () => {
       if (formItems.length > 0) {
         formItems[0].classList.remove('d-none');
         formItems = document.querySelectorAll('.form-item-in.d-none');
-        document.getElementById('map').src += '';
+        map.src += '';
       }
     });
   }
