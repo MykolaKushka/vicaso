83ca00e04be5611815b0b951a830dae169d96554
Mykola Kushka <Mykola.Kushka@gmail.com>
2023-08-05 10:36:15 +0300

    Address page changes

--- src/js/nextBtn.js
+++ src/js/nextBtn.js
@@ -7,14 +7,59 @@ const nextBtn = () => {
   ) {
     const nextBtn = document.querySelector('#nextBtn');
 
-    let formItems = document.querySelectorAll('.form-item-in.d-none');
+    let formItemsVisible = document.querySelectorAll(
+      '#addressArea .form-item-shown .form-control'
+    );
+
+    let formItemsHidden = document.querySelectorAll(
+      '#addressArea .form-item-in.height-0'
+    );
 
     const map = document.getElementById('map');
 
+    const checkIfInputsValued = () => {
+      let value = true;
+      formItemsVisible.forEach((el) => {
+        if (el.value.length < 1) {
+          value = false;
+        }
+        if (el.tagName.toLowerCase() === 'select' && el.value === '0') {
+          value = false;
+        }
+        if (value) nextBtn.removeAttribute('disabled');
+        else nextBtn.setAttribute('disabled', '');
+      });
+    };
+
+    checkIfInputsValued();
+
+    const inputEvent = () => {
+      formItemsVisible.forEach((el) => {
+        el.addEventListener('input', () => {
+          checkIfInputsValued();
+        });
+      });
+    };
+
+    inputEvent();
+
     nextBtn.addEventListener('click', () => {
-      if (formItems.length > 0) {
-        formItems[0].classList.remove('d-none');
-        formItems = document.querySelectorAll('.form-item-in.d-none');
+      if (formItemsHidden.length > 0) {
+        formItemsHidden[0].classList.remove('height-0');
+        formItemsHidden[0].classList.add('form-item-shown');
+        if (formItemsHidden[0].id === 'shootDateArea') {
+          document.getElementById('nextBtnArea').classList.add('d-none');
+          document.getElementById('loginBtnArea').classList.remove('d-none');
+        } else {
+          formItemsHidden = document.querySelectorAll(
+            '#addressArea .form-item-in.height-0'
+          );
+          formItemsVisible = document.querySelectorAll(
+            '#addressArea .form-item-shown .form-control'
+          );
+          checkIfInputsValued();
+          inputEvent();
+        }
         map.src += '';
       }
     });
